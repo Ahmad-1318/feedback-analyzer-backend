d61e57c4dfcb239326351505e85950f492a2b49c
Ahmad <ahmu5522@gmail.com>
2026-03-24 21:49:36 +0500

    Fixed deployment error

--- main.py
+++ main.py
@@ -80,5 +80,8 @@ def health_check():
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
